6d74770198f2cf9d082726c8ed89486235e9e835
Seoyul Kim <zoeyul07@gmail.com>
2022-07-14 17:29:43 +0900

    feat: youtube service 로직 추가

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDom from "react-dom";
 import App from "./App";
 import "./index.css";
+import Youtube from "./service/youtube";
+
+const youtube = new Youtube(process.env.REACT_APP_YOUTUBE_API_KEY);
 
 ReactDom.render(
   <React.StrictMode>
-    <App />
+    <App youtube={youtube} />
   </React.StrictMode>,
   document.getElementById("root")
 );
